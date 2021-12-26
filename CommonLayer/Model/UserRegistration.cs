09b06850d7408e6057bb511425be32a3b49de1bf
“Srujan03076” <srujanmahaja5@gmail.com>
2021-12-26 18:02:09 +0530

    [Srujan]Added: Post API for Login Method

--- CommonLayer/Model/UserRegistration.cs
+++ CommonLayer/Model/UserRegistration.cs
@@ -7,24 +7,40 @@ namespace CommonLayer.Model
 {
    public class UserRegistration
     {
-        
+        /// <summary>
+        /// /// Gets or sets the object named Id of the UserRegistration class
+        /// </summary>
         public long Id { get; set; }
+
+        /// <summary>
+        /// /// Gets or sets the object named Firstname of the UserRegistration class
+        /// </summary>
+        /// 
         [Required(ErrorMessage = "First name is required")]
         [DataType(DataType.Text)]
         [Display(Name = "First Name:")]
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// /// Gets or sets the object named Lastname of the UserRegistration class
+        /// </summary>
         [Required(ErrorMessage = "Last name is required")]
         [DataType(DataType.Text)]
         [Display(Name = "Last Name:")]
         public string LastName { get; set; }
 
+        /// <summary>
+        /// /// Gets or sets the object named EmailId of the UserRegistration class
+        /// </summary>
         [Required(ErrorMessage = "EmailId is required")]
         [DataType(DataType.EmailAddress)]
         [Display(Name = "EmaiId:")]
 
         public string EmailId { get; set; }
 
+        /// <summary>
+        /// /// Gets or sets the object named Password of the UserRegistration class
+        /// </summary>
         [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
         [Display(Name = "Password:")]
