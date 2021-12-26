44e1d218aeb80cea139871f8f426c805cd468420
“Srujan03076” <srujanmahaja5@gmail.com>
2021-12-26 18:21:15 +0530

    [Srujan]Added: Post API for Login Method

--- CommonLayer/UserModel/UserLogin.cs
+++ CommonLayer/UserModel/UserLogin.cs
@@ -5,10 +5,19 @@ using System.Text;
 
 namespace CommonLayer.UserModel
 {
+    /// <summary>
+    /// This is a UserLogin class 
+    /// </summary>
     public class UserLogin
     {
+        /// <summary>
+        /// Gets or sets the email
+        /// </summary>
         [Required]
         public string Email { get; set; }
+        /// <summary>
+        /// Gets or sets the password
+        /// </summary>
         [Required]
         public string Password { get; set; }
     }
