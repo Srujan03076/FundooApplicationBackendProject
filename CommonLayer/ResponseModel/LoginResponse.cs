4e19ea9128fd4fbb2bf0acbee0ef11aa5f533c90
“Srujan03076” <srujanmahaja5@gmail.com>
2021-12-26 18:27:34 +0530

    [Srujan]Added: Post API for Login Method

--- CommonLayer/ResponseModel/LoginResponse.cs
+++ CommonLayer/ResponseModel/LoginResponse.cs
@@ -4,14 +4,34 @@ using System.Text;
 
 namespace CommonLayer.ResponseModel
 {
+    /// <summary>
+    /// This is a LoginResponse class
+    /// </summary>
     public class LoginResponse
     {
-        
+        /// <summary>
+        /// Gets or sets the Id
+        /// </summary>
         public long Id { get; set; }
+        /// <summary>
+        /// Gets or sets the Firstname
+        /// </summary>
         public string FirstName { get; set; }
+        /// <summary>
+        /// Gets or sets the Lirstname
+        /// </summary>
         public string LastName { get; set; }
+        /// <summary>
+        /// Gets or sets the EmailId
+        /// </summary>
         public string EmailId { get; set; }
+        /// <summary>
+        /// Gets or sets the Createat and make it nullable
+        /// </summary>
         public DateTime? Createat { get; set; }
+        /// <summary>
+        /// Gets or sets the Modifiedeat and make it nullable
+        /// </summary>
         public DateTime? Modifiedat { get; set; }
 
     }
