d02edf25e07f5ec5d6a11c6198edf7e132f44efc
“Srujan03076” <srujanmahaja5@gmail.com>
2021-12-28 13:17:01 +0530

    [Srujan]Added: Jwt token for Login Method

--- RepositoryLayer/Services/UserRL.cs
+++ RepositoryLayer/Services/UserRL.cs
@@ -1,11 +1,14 @@
 ﻿using CommonLayer.Model;
 using CommonLayer.ResponseModel;
 using CommonLayer.UserModel;
+using Microsoft.IdentityModel.Tokens;
 using RepositoryLayer.Context;
 using RepositoryLayer.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 
 namespace RepositoryLayer.Services
@@ -15,11 +18,22 @@ namespace RepositoryLayer.Services
     /// </summary>
     public class UserRL : IUserRL
     {
+        private const string SecretKey = "This is my sample key ";
         FundooContext context;
         public UserRL(FundooContext context)
         {
             this.context = context;
         }
+
+        public IEnumerable<User> GetRegistrations()
+        {
+            return context.Users.ToList();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
         public bool Registration(UserRegistration user)
         {
             try
@@ -50,22 +64,29 @@ namespace RepositoryLayer.Services
                 throw;
             }
         }
-
+        /// <summary>
+        /// To check the EmailId, password & Get Login
+        /// </summary>
+        /// <param name="userLogin"></param>
+        /// <returns></returns>
         public LoginResponse UserLogin(UserLogin userLogin)
         {
             try
             {
                 var validateLogin = this.context.Users.Where(x => x.EmailId == userLogin.Email && x.Password == userLogin.Password).FirstOrDefault();
 
-                if (validateLogin.Id != 0 && validateLogin.EmailId != null)  
+                if (validateLogin.Id != 0 && validateLogin.EmailId != null)
                 {
                     LoginResponse login1 = new LoginResponse();
+                    string token;
+                    token = GenerateJWTToken(validateLogin.EmailId);
                     login1.Id = validateLogin.Id;
                     login1.FirstName = validateLogin.FirstName;
                     login1.LastName = validateLogin.LastName;
                     login1.EmailId = validateLogin.EmailId;
                     login1.Createat = validateLogin.Createdat;
                     login1.Modifiedat = validateLogin.Modifiedat;
+                    login1.token =token;
                     return login1;
                 }
                 else
@@ -78,8 +99,38 @@ namespace RepositoryLayer.Services
                 throw;
             }
         }
+        /// <summary>
+        /// This method implements generation of token
+        /// </summary>
+        /// <param name="EmailId"></param>
+        /// <returns></returns>
+        private string GenerateJWTToken(string EmailId)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("This is my sample key"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[] {
+                new Claim("EmailId",EmailId) 
+            };
+                
+             var token = new JwtSecurityToken("Srujan", EmailId, claims,
+              expires: DateTime.Now.AddMinutes(10),
+              signingCredentials: credentials);
+              return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
        
