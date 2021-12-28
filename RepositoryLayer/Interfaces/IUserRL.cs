ff3417f1e97c966b119bb8bc530ee5e453494959
“Srujan03076” <srujanmahaja5@gmail.com>
2021-12-28 13:19:17 +0530

    [Srujan]Added: Get API for Login Method

--- RepositoryLayer/Interfaces/IUserRL.cs
+++ RepositoryLayer/Interfaces/IUserRL.cs
@@ -1,6 +1,7 @@
 ﻿using CommonLayer.Model;
 using CommonLayer.ResponseModel;
 using CommonLayer.UserModel;
+using RepositoryLayer.Context;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -21,6 +22,12 @@ namespace RepositoryLayer.Interfaces
         /// <param name="userLogin"></param>
         /// <returns></returns>
         LoginResponse UserLogin(UserLogin userLogin);
-        
+        /// <summary>
+        /// This methods implements to get all registered data
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<User> GetRegistrations();
+
+
     }
 }
