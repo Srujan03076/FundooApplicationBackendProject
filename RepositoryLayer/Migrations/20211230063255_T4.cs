65d6e3a627907fe3c27fc6647a9f52d5e0e1df96
“Srujan03076” <srujanmahaja5@gmail.com>
2021-12-31 08:35:40 +0530

    [Srujan]Added: Creating ,GettingAllData,Deleting for Note Registration

---
+++ RepositoryLayer/Migrations/20211230063255_T4.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepositoryLayer.Migrations
+{
+    public partial class T4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserTable",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    EmailId = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(maxLength: 30, nullable: false),
+                    Createdat = table.Column<DateTime>(nullable: true),
+                    Modifiedat = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTable", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NoteTable",
+                columns: table => new
+                {
+                    NotesId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: false),
+                    Message = table.Column<string>(nullable: false),
+                    Remainder = table.Column<DateTime>(nullable: false),
+                    Colour = table.Column<string>(nullable: false),
+                    Image = table.Column<string>(nullable: false),
+                    IsArchive = table.Column<bool>(nullable: false),
+                    IsPin = table.Column<bool>(nullable: false),
+                    IsTrash = table.Column<bool>(nullable: false),
+                    Createdat = table.Column<DateTime>(nullable: true),
+                    Modifiedat = table.Column<DateTime>(nullable: true),
+                    Id = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NoteTable", x => x.NotesId);
+                    table.ForeignKey(
+                        name: "FK_NoteTable_UserTable_Id",
+                        column: x => x.Id,
+                        principalTable: "UserTable",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NoteTable_Id",
+                table: "NoteTable",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NoteTable");
+
+            migrationBuilder.DropTable(
+                name: "UserTable");
+        }
+    }
+}
---
+++ RepositoryLayer/Services/NotesRL.cs
@@ -0,0 +1,119 @@
+﻿using CommonLayer.Model;
+using RepositoryLayer.Context;
+using RepositoryLayer.Entities;
+using RepositoryLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RepositoryLayer.Services
+{
+    public class NotesRL : INotesRL
+    {
+        FundooContext context;
+        public NotesRL(FundooContext context)
+        {
+            this.context = context;
+        }
+
+        public bool DeleteNotes(DeleteNote deletenotes)
+        {
+            {
+                try
+                {
+                    var ValidNote = this.context.NoteTable.Where(Y => Y.Id == deletenotes.Id).FirstOrDefault();
+                    if (ValidNote != null)
+                    {
+                        Notes deleteNote = new Notes();
+                        deleteNote.Title = ValidNote.Title;
+                        deleteNote.Message = ValidNote.Message;
+                        deleteNote.Remainder = ValidNote.Remainder;
+                        deleteNote.Colour = ValidNote.Colour;
+                        deleteNote.Image = ValidNote.Image;
+                        deleteNote.IsArchive = ValidNote.IsArchive;
+                        deleteNote.IsPin = ValidNote.IsPin;
+                        deleteNote.IsTrash = ValidNote.IsTrash;
+                        deleteNote.Createdat = ValidNote.Createdat;
+                        deleteNote.Modifiedat = ValidNote.Modifiedat;
+                    }
+                    //Deleting user details from the database user table 
+                    this.context.NoteTable.Remove(ValidNote);
+
+                    int result = this.context.SaveChanges();
+                    if (result > 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+            }
+        }
+
+        public IEnumerable<Notes> GetAllNotesData()
+        {
+            return context.NoteTable.ToList();
+        }
+
+       
+
+        public bool MakeANote(UserNotes notes)
+        {
+            try
+            {
+                Notes newNotes = new Notes();
+                newNotes.Title = notes.Title;
+                newNotes.Message = notes.Message;
+                newNotes.Remainder = notes.Remainder;
+                newNotes.Colour = notes.Colour;
+                newNotes.Image = notes.Image;
+                newNotes.IsArchive = notes.IsArchive;
+                newNotes.IsPin = notes.IsPin;
+                newNotes.IsTrash = notes.IsTrash;
+                newNotes.Id = notes.Id;
+                newNotes.Createdat = notes.Createdat;
+                //Adding the data to database
+                this.context.NoteTable.Add(newNotes);
+                //Save the changes in database
+                int result = this.context.SaveChanges();
+                if (result > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
+        
+
+    
+               
+                   
+                
+                   
+
+                
+
+
+
+
+
+
+
+
