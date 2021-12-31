a64f3149dca29110b7dca039d346195a9e49b1b7
“Srujan03076” <srujanmahaja5@gmail.com>
2021-12-31 08:17:42 +0530

    [Srujan]Added: Creating ,GettingAllData,Deleting for Note Registration

---
+++ BussinessLayer/Interfaces/INotesBL.cs
@@ -0,0 +1,15 @@
+﻿using CommonLayer.Model;
+using RepositoryLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BussinessLayer.Interfaces
+{
+    public interface INotesBL
+    {
+        public bool MakeANote(UserNotes notes);
+        IEnumerable<Notes> GetAllNotesData();
+        public bool DeleteNotes(DeleteNote deletenotes);
+    }
+}
