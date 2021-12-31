4d3dac4991c4569d15fac12127e9811bc7f14ab9
“Srujan03076” <srujanmahaja5@gmail.com>
2021-12-31 08:30:36 +0530

    [Srujan]Added: Adding Entities for Notes

---
+++ RepositoryLayer/Entities/Notes.cs
@@ -0,0 +1,46 @@
+﻿using RepositoryLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RepositoryLayer.Entities
+{
+    public class Notes
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int NotesId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Message { get; set; }
+        [Required]
+        public DateTime Remainder { get; set; }
+        [Required]
+        public string Colour { get; set; }
+        [Required]
+        public string Image { get; set; }
+        [Required]
+        public bool IsArchive { get; set; }
+        [Required]
+        public bool IsPin { get; set; }
+        [Required]
+        public bool IsTrash { get; set; }
+        public DateTime? Createdat { get; set; }
+        public DateTime? Modifiedat { get; set; }
+        //[Required]
+        //[ForeignKey("UserTable")]
+        public User User { get; set; }
+
+        [ForeignKey("User")]
+       public long Id { get; set; }
+    }
+}
+      
+
+
+
+
+        
