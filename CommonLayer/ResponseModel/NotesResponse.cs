768f9449c86914a5a07f928f67f0a46b11bda838
“Srujan03076” <srujanmahaja5@gmail.com>
2021-12-31 08:22:17 +0530

    [Srujan]Added: Creating ,GettingAllData,Deleting for Note Registration

---
+++ CommonLayer/ResponseModel/NotesResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.ResponseModel
+{
+    class NotesResponse
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public DateTime Remainder { get; set; }
+        public string Colour { get; set; }
+        public string Image { get; set; }
+        public bool IsArchive { get; set; }
+        public bool IsPin { get; set; }
+        public bool IsTrash { get; set; }
+        public long Id { get; set; }
+    }
+}
