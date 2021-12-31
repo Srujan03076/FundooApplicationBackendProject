da03ba43d0d006eadd73aaf91be10c714e0d7e5a
“Srujan03076” <srujanmahaja5@gmail.com>
2021-12-31 08:19:13 +0530

    [Srujan]Added: Creating ,GettingAllData,Deleting for Note Registration

---
+++ BussinessLayer/Services/NotesBL.cs
@@ -0,0 +1,53 @@
+﻿using BussinessLayer.Interfaces;
+using CommonLayer.Model;
+using RepositoryLayer.Entities;
+using RepositoryLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BussinessLayer.Services
+{
+   public class NotesBL : INotesBL
+    {
+
+        INotesRL notesRL;
+        public NotesBL(INotesRL notesRL)
+        {
+            this.notesRL = notesRL;
+
+        }
+
+        public bool DeleteNotes(DeleteNote deletenotes)
+        {
+            try
+            {
+                return this.notesRL.DeleteNotes(deletenotes);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+        
+        public IEnumerable<Notes> GetAllNotesData()
+        {
+            return this.notesRL.GetAllNotesData();
+        }
+
+        public bool MakeANote(UserNotes notes)
+        {
+            try
+            {
+                return this.notesRL.MakeANote(notes);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
+        }
+
+        
+    }
+}
