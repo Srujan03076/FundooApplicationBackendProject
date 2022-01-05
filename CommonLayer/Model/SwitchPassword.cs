0d4dffb32af17bd72f8bd113525506206a59739f
“Srujan03076” <srujanmahaja5@gmail.com>
2022-01-05 08:04:42 +0530

    [Srujan]Added: Forgot Password and Reset Password API

---
+++ CommonLayer/Model/SwitchPassword.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Model
+{
+    public class SwitchPassword
+    {
+        public string EmailId { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
+    
