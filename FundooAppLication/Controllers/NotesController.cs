1931fe9e28b81483ddc57b17b05f78d2b920ba98
“Srujan03076” <srujanmahaja5@gmail.com>
2022-01-07 08:02:46 +0530

    [Srujan]Added: Color,Pin,Trash,Archive API for Notes

--- FundooAppLication/Controllers/NotesController.cs
+++ FundooAppLication/Controllers/NotesController.cs
@@ -41,6 +41,7 @@ namespace FundooAppLication.Controllers
                 return this.BadRequest(new { Success = false, message = e.InnerException });
             }
         }
+        [Authorize]
         [HttpGet("getalldata")]
         public IActionResult GetAllNotesData()
         {
@@ -58,14 +59,14 @@ namespace FundooAppLication.Controllers
                 return this.BadRequest(new { Success = false, message = e.InnerException });
             }
         }
-        [HttpPut("{id}")]
-        public IActionResult UpdateNotes( UserNotes usernotes )
+        [HttpPut("Update")]
+        public IActionResult UpdateNotes(UserNotes usernotes)
         {
             try
             {
                 UserNotes userresponse = notesBL.UpdateNotes(usernotes);
                 if (userresponse != null)
-                    
+
                 {
                     return this.Ok(new { Success = true, message = " Updation Succesful", });
                 }
@@ -80,7 +81,7 @@ namespace FundooAppLication.Controllers
             }
         }
         [HttpDelete("Delete")]
-        public IActionResult DeleteNotes( long Id)
+        public IActionResult DeleteNotes(long Id)
         {
             try
             {
@@ -98,6 +99,135 @@ namespace FundooAppLication.Controllers
                 return this.BadRequest(new { success = false, message = e.InnerException });
             }
         }
+        [HttpPut("Archive")]
+        public IActionResult IsArchive(int notesId)
+        {
+            try
+            {
+                 var result = this.notesBL.IsArchive(notesId);
+                if (result == true)
+                {
+                    return Ok(new { Success = true, message = "Note archived Successfully." });
+                }
+                else
+                {
+                    return BadRequest(new { Success = false, message = "Unsuccessful" });
+                }
+            }
+            catch (Exception e)
+            {
+                return this.BadRequest(new { success = false, message = e.InnerException });
+            }
+        }
+        [HttpPut("Unarchive")]
+  
+        public IActionResult Unarchive(int notesId)
+        {
+            try
+            {
+                var result = this.notesBL.UnArchive(notesId);
+                if (result == true)
+                {
+                    return Ok(new { Success = true, message = "Note Unarchived Successfully." });
+                }
+                else
+                {
+                    return BadRequest(new { Success = false, message = "Unsuccessful" });
+                }
+            }
+            catch (Exception e)
+            {
+                return this.BadRequest(new { success = false, message = e.InnerException });
+            }
+        }
+
+        [HttpPut("Pinnote")]
+        public IActionResult NoteAddtionAsPinned(int notesId, long id)
+        {
+            try
+            {
+                var result = notesBL.NoteAddtionAsPinned(notesId, id);
+                if (result == true)
+                {
+                    return Ok(new { success = true, message = "Pinned Successfully" });
+                }
+                else
+                {
+                    return BadRequest(new { success = false, message = "Unsuccessful" });
+                }
+            }
+            catch (Exception e)
+            {
+                return this.BadRequest(new { success = false, message = e.InnerException });
+            }
+        }
+
+        [HttpDelete("Trash")]
+        public IActionResult Trash(int notesId)
+        {
+            try
+            {
+                var result = this.notesBL.Trash(notesId);
+                if (result == true)
+                {
+                    return Ok(new { Success = true, message = "Note moved to trash Successfully." });
+                }
+                else
+                {
+                    return BadRequest(new { Success = false, message = "Unsuccesful" });
+                }
+            }
+            catch (Exception e)
+            {
+                return this.BadRequest(new { success = false, message = e.InnerException });
+            }
+        }
+            
+        
+
+        [HttpPut("RestoreTrash")]
+        public IActionResult RestoreTrash(int notesId)
+        {
+            try
+            {
+                var result = this.notesBL.RestoreTrash(notesId);
+                if (result == true)
+                {
+                    return Ok(new { Success = true, message = "Note restored from trash Successfully." });
+                }
+                else
+                {
+                    return BadRequest(new { Success = false, message = "Unsuccessful" });
+                }
+            }
+            catch (Exception e)
+            {
+                return this.BadRequest(new { success = false, message = e.InnerException });
+            }
+        }
+
+
+        [HttpPut("ChangeColour")]
+        public IActionResult EditColour(int notesId, string colour)
+        {
+
+            try
+            {
+                var result = this.notesBL.EditColour(notesId, colour);
+                if (result == true)
+                {
+                    return Ok(new { success = true, message = "Colour changed Successfully !!" });
+                }
+                else
+                {
+                    return BadRequest(new { success = false, message = "Colour not changed !!" });
+                }
+            }
+            catch (Exception e)
+            {
+                return this.BadRequest(new { success = false, message = e.InnerException }); ;
+            }
+        }
     }
 }
 
@@ -106,10 +236,12 @@ namespace FundooAppLication.Controllers
 
 
 
-            
 
-       
 
-    
-   
+
+
+
+
+
+
 
