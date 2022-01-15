feac4e392a002d84b521c286b26b4fe679f04a27
“Srujan03076” <srujanmahaja5@gmail.com>
2022-01-15 07:57:38 +0530

    [Srujan]Added: Collaborator API for notes

---
+++ BussinessLayer/Services/CollaboratorBL.cs
@@ -0,0 +1,50 @@
+﻿using BussinessLayer.Interfaces;
+using CommonLayer.Model;
+using RepositoryLayer.Entities;
+using RepositoryLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BussinessLayer.Services
+{
+   public class CollaboratorBL : ICollaboratorBL
+    {
+        ICollaboratorRL collaboratorRL;
+        public CollaboratorBL(ICollaboratorRL collaboratorRL)
+        {
+            this.collaboratorRL = collaboratorRL;
+
+        }
+        public CollabModel AddCollab(CollabModel collaboration, long tokenId)
+        {
+                try
+                {
+                    return this.collaboratorRL.AddCollab(collaboration, tokenId);
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+
+            }
+        public bool Deletecollab(CollabModel model)
+        {
+            try
+            {
+                return this.collaboratorRL.DeleteCollab(model);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
+        }
+
+        public IEnumerable<Collaborator> GetCollaborator()
+        {
+            return this.collaboratorRL.GetCollaborator();
+        }
+
+    }
+}
