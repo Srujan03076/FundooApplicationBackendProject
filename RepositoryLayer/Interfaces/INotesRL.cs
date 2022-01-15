3a96ec67502abbc427cbbdbb014d67ff28a0cd7e
“Srujan03076” <srujanmahaja5@gmail.com>
2022-01-15 08:20:13 +0530

    [Srujan]Added: Refactor the Code

--- RepositoryLayer/Interfaces/INotesRL.cs
+++ RepositoryLayer/Interfaces/INotesRL.cs
@@ -1,4 +1,5 @@
 ﻿using CommonLayer.Model;
+using Microsoft.AspNetCore.Http;
 using RepositoryLayer.Entities;
 using System;
 using System.Collections.Generic;
@@ -8,16 +9,74 @@ namespace RepositoryLayer.Interfaces
 {
     public interface INotesRL
     {
-        public bool MakeANote(UserNotes notes);
+        /// <summary>
+        /// Creating the notes for user
+        /// </summary>
+        /// <param name="notes"></param>
+        /// <returns></returns>
+        UserNotes MakeANote(UserNotes notes);
+        /// <summary>
+        /// Getting all the data from database
+        /// </summary>
+        /// <returns></returns>
         IEnumerable<Notes> GetAllNotesData();
+        /// <summary>
+        /// Deleting the notes
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <returns></returns>
         public bool DeleteNotes(long Id);
+        /// <summary>
+        ///  Updating the notes 
+        /// </summary>
+        /// <param name="usernotes"></param>
+        /// <returns></returns>
         public UserNotes UpdateNotes(UserNotes usernotes);
+        /// <summary>
+        /// Make a note as pinned
+        /// </summary>
+        /// <param name="notesId"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public bool NoteAdditionAsPinned(int notesId, long id);
+        /// <summary>
+        /// Make a note moved to trash
+        /// </summary>
+        /// <param name="notesId"></param>
+        /// <returns></returns>
         public bool Trash(int notesId);
+        /// <summary>
+        /// Make the note archive
+        /// </summary>
+        /// <param name="notesId"></param>
+        /// <returns></returns>
         public bool IsArchive(int notesId);
+        /// <summary>
+        /// Change the color of note
+        /// </summary>
+        /// <param name="notesId"></param>
+        /// <param name="colour"></param>
+        /// <returns></returns>
         public bool EditColour(int notesId, string colour);
+        /// <summary>
+        /// Make the note unarchive
+        /// </summary>
+        /// <param name="notesId"></param>
+        /// <returns></returns>
         public bool UnArchive(int notesId);
+        /// <summary>
+        /// Restore trash from notes
+        /// </summary>
+        /// <param name="notesId"></param>
+        /// <returns></returns>
         public bool RestoreTrash(int notesId);
+        /// <summary>
+        /// Add Image in notes
+        /// </summary>
+        /// <param name="notesId"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public bool AddImage(long notesId, IFormFile path);
     }
 
 }
