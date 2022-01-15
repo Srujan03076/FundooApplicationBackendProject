e4a0b19dc16c28e5be9b817df356089ef6db138d
“Srujan03076” <srujanmahaja5@gmail.com>
2022-01-15 08:21:20 +0530

    [Srujan]Added: Label API for notes

---
+++ RepositoryLayer/Interfaces/ILabelRL.cs
@@ -0,0 +1,37 @@
+﻿using CommonLayer.Model;
+using RepositoryLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interfaces
+{
+    public interface ILabelRL
+    {
+        /// <summary>
+        /// Adding the label into notes
+        /// </summary>
+        /// <param name="labelModel"></param>
+        /// <returns></returns>
+        LabelModel MakeALabel(LabelModel labelModel);
+        /// <summary>
+        /// Make changes into label
+        /// </summary>
+        /// <param name="labelId"></param>
+        /// <param name="labelModel"></param>
+        /// <returns></returns>
+        public LabelModel EditLabel(long labelId, LabelModel labelModel);
+        /// <summary>
+        /// Deleting the label from notes
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public bool DeleteLabel(LabelModel model);
+        /// <summary>
+        /// Getting All the data
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Label> Getlabels();
+       
+    }
+}
