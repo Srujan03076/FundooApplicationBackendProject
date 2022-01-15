a346f2605efbf5db3273b9bcb5d2e724bb14ad55
“Srujan03076” <srujanmahaja5@gmail.com>
2022-01-15 08:18:38 +0530

    [Srujan]Added: Adding Entities for Label API

---
+++ RepositoryLayer/Entities/Label.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RepositoryLayer.Entities
+{
+    public class Label
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long LabelId { get; set; }
+        public string LabelName { get; set; }
+        public Notes notes { get; set; }
+        public int NotesId { get; set; }
+        public long Id { get; set; }
+    }
+}
