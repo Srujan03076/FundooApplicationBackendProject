f9074957d393279547b22435d2b11962e6c9bfc9
“Srujan03076” <srujanmahaja5@gmail.com>
2022-01-15 08:18:13 +0530

    [Srujan]Added: Adding Entities for Collaborator API

---
+++ RepositoryLayer/Entities/Collaborator.cs
@@ -0,0 +1,21 @@
+﻿using RepositoryLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RepositoryLayer.Entities
+{
+    public class Collaborator
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long CollaboratorID { get; set; }
+        public string EmailId { get; set; }
+        public Notes notes { get; set; }
+        public int NotesId { get; set; }
+        public User User { get; set; }
+        public long Id { get; set; }
+    }
+}
