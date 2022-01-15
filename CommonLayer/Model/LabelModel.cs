c30f953143f68b0671524490a2655b701b7eb27d
“Srujan03076” <srujanmahaja5@gmail.com>
2022-01-15 08:04:25 +0530

    [Srujan]Added: Label API for notes

---
+++ CommonLayer/Model/LabelModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.Model
+{
+    public class LabelModel
+    {
+        public long Id { get; set; }
+        public string LabelName { get; set; }
+        public int NotesId { get; set; }
+    }
+}
+
+
